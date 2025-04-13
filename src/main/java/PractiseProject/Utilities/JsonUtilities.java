7f69f0479c7220ae0fce80851f48c9d49bf1ae9d
khaledtouseff <khaledyoussouph36@gmail.com>
2025-04-14 00:13:18 +0200

    adding json utilities class with its constructor and method

---
+++ src/main/java/PractiseProject/Utilities/JsonUtilities.java
@@ -0,0 +1,38 @@
+package PractiseProject.Utilities;
+
+import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+
+public class JsonUtilities {
+    public static final String JSON_FILE_PATH = "src/test/resources";
+    String jsonReader;
+    String jsonFileName;
+
+
+    // I created this object to recall it whenever i want to use a data from
+    public JsonUtilities(String jsonFileName) {
+        this.jsonFileName = jsonFileName;
+        try {
+            JSONObject data = (JSONObject) new JSONParser().parse(new FileReader(JSON_FILE_PATH + jsonFileName + ".json"));
+            jsonReader = data.toJSONString();
+        } catch (Exception e) {
+            LogsUtilities.error(e.getMessage());
+        }
+    }
+
+    // this method is to actually get the data
+    public String getJsonData(String jsonPath) {
+        String testData = "";
+        try {
+            testData = JsonPath.read(jsonReader, jsonPath);
+        } catch (Exception e) {
+            LogsUtilities.error(e.getMessage(), " NO result for json path: " + jsonPath + " in the json file:", this.jsonFileName + "'" );
+        }
+        LogsUtilities.info("Json path: '" + jsonPath + "' in the json file: '" + this.jsonFileName + "' has value: '" + testData + "'");
+
+        return testData;
+    }
+}
